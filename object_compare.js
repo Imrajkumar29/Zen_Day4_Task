669a12ff6b1f788d4a8c7381be88d7a30eab1737
Imrajkumar29 <rajkumar291994@gmail.com>
2023-03-14 16:39:29 +0530

    object comparison

--- object_compare.js
+++ object_compare.js
@@ -10,4 +10,20 @@ var object2 = {
 
 console.log(JSON.stringify(object1));
 console.log(JSON.stringify(object1) === JSON.stringify(object2));
-console.log(_.isEqual(object1, object2));
+
+const order1 = {};
+const order2 = {};
+
+Object.keys(object1)
+  .sort()
+  .forEach((key) => {
+    order1[key] = object1[key];
+  });
+
+Object.keys(object2)
+  .sort()
+  .forEach((key) => {
+    order2[key] = object2[key];
+  });
+
+console.log(JSON.stringify(order1) === JSON.stringify(order2));
